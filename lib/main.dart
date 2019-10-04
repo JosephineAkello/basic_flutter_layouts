cd300c80f845a4bc1414b78abb0f95554f8c41b6
mj <onyangomaureen95@gmail.com>
2019-10-04 18:44:20 +0300

    change to a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,26 @@ void main(){
   runApp(LayoutApp());
 }
 
-class LayoutApp extends StatelessWidget{
-  Widget build(context){
-    return MaterialApp();
+class LayoutApp extends StatefulWidget{
+  createState(){
+    return LayoutAppState();
   }
+}
+class LayoutAppState extends State<LayoutApp>{
+  Widget build(context){
+    return MaterialApp(
+      title: 'Flutter layout Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout Example'),
+        ),
+        body: Center(
+          child: Text('HelloWorld'),
+        ),
+      ),
+    );
+  }
+  Widget titleSection = Container(
+
+  );
 }
