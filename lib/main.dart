38068bbee271834eeaf4ac084590c8bb2fd5a84d
mj <onyangomaureen95@gmail.com>
2019-10-04 20:54:16 +0300

    place a buildColumns private helper method

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(LayoutApp());
 }
 
-class LayoutApp extends StatefulWidget{
-  createState(){
+class LayoutApp extends StatefulWidget {
+  createState() {
     return LayoutAppState();
   }
 }
-class LayoutAppState extends State<LayoutApp>{
-    Widget titleSection = Container(
+
+class LayoutAppState extends State<LayoutApp> {
+  Widget titleSection = Container(
     padding: EdgeInsets.all(32),
     child: Row(
       children: <Widget>[
@@ -22,38 +23,54 @@ class LayoutAppState extends State<LayoutApp>{
                 padding: EdgeInsets.only(bottom: 8.0),
                 child: Text(
                   'Kisumu Finest',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
               ),
-              Text("Kisumu's, Kenya",
-              style: TextStyle(color:
-              Colors.grey)),
+              Text("Kisumu's, Kenya", style: TextStyle(color: Colors.grey)),
             ],
           ),
-          
         ),
-        Icon(Icons.star,
-        color: Colors.red,),
+        Icon(
+          Icons.star,
+          color: Colors.red,
+        ),
         Text('40')
       ],
     ),
-
   );
-  Widget build(context){
+  Widget build(context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.indigo),
       title: 'Flutter layout Example',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter layout Example'),
         ),
-        body: Column(
-          children: [
-            titleSection,
-          ]),
+        body: Column(children: [
+          titleSection,
+        ]),
       ),
     );
   }
+  Column _buildColumns(Color color, IconData icon, String label){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Icon(icon, color: color,),
+        Container(
+          margin: EdgeInsets.only(top: 8.0),
+          child: Text(
+            label,
+            style: TextStyle(fontSize: 12,
+            fontWeight: FontWeight.bold,color: color)
+          ),
+        )
+      ],
+    );
+   
+  }
+
 
 }
