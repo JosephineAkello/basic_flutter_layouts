02abdaa7f66a0f012358090032895dda81e6007b
mj <onyangomaureen95@gmail.com>
2019-10-04 20:34:58 +0300

    add description contents of title section

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,37 @@ class LayoutApp extends StatefulWidget{
   }
 }
 class LayoutAppState extends State<LayoutApp>{
+    Widget titleSection = Container(
+    padding: EdgeInsets.all(32),
+    child: Row(
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  'Kisumu Finest',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              Text('Kisumu, Kenya',
+              style: TextStyle(color:
+              Colors.grey)),
+            ],
+          ),
+          
+        ),
+        Icon(Icons.star,
+        color: Colors.red,),
+        Text('40')
+      ],
+    ),
+
+  );
   Widget build(context){
     return MaterialApp(
       title: 'Flutter layout Example',
@@ -23,7 +54,5 @@ class LayoutAppState extends State<LayoutApp>{
       ),
     );
   }
-  Widget titleSection = Container(
 
-  );
 }
