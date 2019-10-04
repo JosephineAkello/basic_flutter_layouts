34e4f196fabc3b412a190baca3d831fa47f4a5da
mj <onyangomaureen95@gmail.com>
2019-10-05 00:36:53 +0300

    resize image shape

--- lib/main.dart
+++ lib/main.dart
@@ -11,59 +11,59 @@ class LayoutApp extends StatefulWidget {
 }
 
 class LayoutAppState extends State<LayoutApp> {
- Widget build(context) {
+  Widget build(context) {
     Widget titleSection = Container(
-    padding: EdgeInsets.all(32),
-    child: Row(
-      children: <Widget>[
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(bottom: 8.0),
-                child: Text(
-                  'Kisumu Finest',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+      padding: EdgeInsets.all(32),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    'Kisumu Finest',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-              Text("Kisumu's, Kenya", style: TextStyle(color: Colors.grey)),
-            ],
+                Text("Kisumu's, Kenya", style: TextStyle(color: Colors.grey)),
+              ],
+            ),
           ),
-        ),
-        Icon(
-          Icons.star,
-          color: Colors.red,
-        ),
-        Text('40')
-      ],
-    ),
-  );
-  Color color = Theme.of(context).primaryColor;
+          Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          Text('40')
+        ],
+      ),
+    );
+    Color color = Theme.of(context).primaryColor;
 
-Widget buttonSection = Container(
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      _buildButtonColumn(color, Icons.call, 'CALL'),
-      _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-      _buildButtonColumn(color, Icons.share, 'SHARE'),
-    ],
-  ),
-);
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
 
-Widget textSection = Container(
-  padding: EdgeInsets.all(32.0),
-  child: Text(
-     'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+    Widget textSection = Container(
+      padding: EdgeInsets.all(32.0),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
         'Alps. Situated 1,578 meters above sea level, it is one of the '
         'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
         'half-hour walk through pastures and pine forest, leads you to the '
         'lake, which warms to 20 degrees Celsius in the summer. Activities '
         'enjoyed here include rowing, and riding the summer toboggan run.',
-     softWrap: true,
-  ),
-);
+        softWrap: true,
+      ),
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.indigo),
@@ -73,10 +73,12 @@ Widget textSection = Container(
           title: Text('Flutter layout Example'),
         ),
         body: ListView(children: [
-          Image.asset('images/kisumufinest.jpg',
-          width: 600,
-          height: 280,
-          fit: BoxFit.cover,),
+          Image.asset(
+            'images/kisumufinest.jpg',
+            width: 600,
+            height: 370,
+            fit: BoxFit.cover,
+          ),
           titleSection,
           buttonSection,
           textSection
@@ -84,27 +86,23 @@ Widget textSection = Container(
       ),
     );
   }
-  
 
-  Column _buildButtonColumn(Color color, IconData icon, String label){
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        Icon(icon, color: color,),
+        Icon(
+          icon,
+          color: color,
+        ),
         Container(
           margin: EdgeInsets.only(top: 8.0),
-          child: Text(
-            label,
-            style: TextStyle(fontSize: 12,
-            fontWeight: FontWeight.bold,color: color)
-          ),
+          child: Text(label,
+              style: TextStyle(
+                  fontSize: 12, fontWeight: FontWeight.bold, color: color)),
         )
       ],
     );
-   
   }
- 
-  }
-
-
+}
