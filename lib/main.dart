11d86dfab16431bb5566919b76cea21d53b296a8
mj <onyangomaureen95@gmail.com>
2019-10-04 21:55:03 +0300

    place textsection declaration

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class LayoutApp extends StatefulWidget {
 }
 
 class LayoutAppState extends State<LayoutApp> {
-  Widget titleSection = Container(
+ Widget build(context) {
+    Widget titleSection = Container(
     padding: EdgeInsets.all(32),
     child: Row(
       children: <Widget>[
@@ -38,7 +39,31 @@ class LayoutAppState extends State<LayoutApp> {
       ],
     ),
   );
-  Widget build(context) {
+  Color color = Theme.of(context).primaryColor;
+
+Widget buttonSection = Container(
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      _buildButtonColumn(color, Icons.call, 'CALL'),
+      _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+      _buildButtonColumn(color, Icons.share, 'SHARE'),
+    ],
+  ),
+);
+
+Widget textSection = Container(
+  padding: EdgeInsets.all(32.0),
+  child: Text(
+     'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+     softWrap: true,
+  ),
+);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.indigo),
@@ -49,11 +74,15 @@ class LayoutAppState extends State<LayoutApp> {
         ),
         body: Column(children: [
           titleSection,
+          buttonSection,
+          textSection
         ]),
       ),
     );
   }
-  Column _buildColumns(Color color, IconData icon, String label){
+  
+
+  Column _buildButtonColumn(Color color, IconData icon, String label){
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       mainAxisSize: MainAxisSize.min,
@@ -71,6 +100,7 @@ class LayoutAppState extends State<LayoutApp> {
     );
    
   }
+ 
+  }
 
 
-}
